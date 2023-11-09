e1f5a5a54a7fb4f2696cd8e071e93fc47ddd974a
heshamcodac <heshamabdulzaher@gmail.com>
2023-11-09 18:05:29 +0200

    init the app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //   webpack: (config) => {
+  //     config.resolve.alias["@components"] = path.join(
+  //       __dirname,
+  //       "./app/components"
+  //     );
+  //     return config;
+  //   },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
